d7462ff3c9e7b2f58ac19fd6429355fa3560d777
Vineet10804 <131114297+Vineet10804@users.noreply.github.com>
2024-03-11 21:34:49 +0530

    Add files via upload

--- DSA/average.c
+++ DSA/average.c
@@ -2,17 +2,21 @@
 int main()
 {
     int n;
-    printf("Average of how many numbers to be calculated:\n");
-    scanf("%d",&n);
-    float a[n];
-    int i;
-    printf("Enter %d numbers:\n",n);
-    float result=0;
-    for(i=0;i<n;i++)
+    while(1)
     {
-        scanf("%f",&a[i]);
-        result=result+a[i];
+        printf("Average of how many numbers to be calculated:\n");
+        scanf("%d",&n);
+        float a[n];
+        int i;
+        printf("Enter %d numbers:\n",n);
+        float result=0;
+        for(i=0;i<n;i++)
+        {
+            scanf("%f",&a[i]);
+            result=result+a[i];
+        }
+        result=result/n;
+        printf("The average of three numbers is:%f\n",result);
     }
-    result=result/n;
-    printf("The average of three numbers is:%f",result);
+
 }
